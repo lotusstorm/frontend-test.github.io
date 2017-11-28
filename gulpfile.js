6572f083e9c75f593e0168636ebf7284fe616dbf
lotusstorm <iutizev@gmail.com>
2017-11-28 04:30:23 +0300

    start commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const gulp         = require('gulp');
 const less         = require('gulp-less');
 const browserSync  = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
-// const rename       = require('gulp-rename');
-const pug          = require('gulp-pug');
+const rename       = require('gulp-rename');
+const ejs          = require('gulp-ejs');
+const gutil        = require('gulp-util');
 const sourcemaps   = require('gulp-sourcemaps');
 const concat       = require('gulp-concat');
 const uglify       = require('gulp-uglify-es').default;
 const imagemin     = require('gulp-imagemin');
 const cssmin       = require('gulp-clean-css');
 const gulpIf       = require('gulp-if');
+const data         = require('./src/data');
 
 const isProdaction = process.env.NODE_ENV == 'prodaction';
 
@@ -30,7 +32,7 @@ gulp.task('livereload', () => {
 });
 
 gulp.task('styles', () => {
-    gulp.src('src/main.less')
+    gulp.src('src/less/main.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(autoprefixer())
@@ -46,7 +48,7 @@ gulp.task('img', () => {
 });
 
 gulp.task('js', () => {
-    gulp.src(['src/**/*.js', '!src/_assets/**/*.js'])
+    gulp.src('src/js/*.js')
         .pipe(sourcemaps.init())
         .pipe(concat('main.js'))
         .pipe(gulpIf(isProdaction, uglify()))
@@ -55,18 +57,24 @@ gulp.task('js', () => {
 });
 
 gulp.task('html', () => {
-    gulp.src(['src/*.pug', '!src/template.pug' ])
-        .pipe(pug({pretty: true}))
+    gulp.src('src/index.ejs')
+        .pipe(ejs(data).on('error', gutil.log))
+        .pipe(rename('index.html'))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('fonts', () => {
+    gulp.src('src/fonts/*.*')
+        .pipe(gulp.dest('./dist/fonts'));
 });
 
 // Отслеживание изменений в файлах, нужно только при локальной разработке
 gulp.task('watch', () => {
-    gulp.watch('src/**/*.less', ['styles']);
-    gulp.watch('src/**/*.pug', ['html']);
+    gulp.watch('src/less/*.less', ['styles']);
+    gulp.watch('src/**/*.ejs', ['html']);
     gulp.watch('src/img/**/*.*', ['img']);
-    gulp.watch('src/**/*.*', ['js']);
+    gulp.watch('src/js/*.js', ['js']);
 });
 
-gulp.task('default', ['styles', 'html', 'img', 'js', 'livereload', 'watch']);
-gulp.task('prod', ['styles', 'html', 'img', 'js']);
+gulp.task('default', ['livereload', 'watch', 'styles', 'html', 'img', 'js', 'fonts']);
+gulp.task('prod', ['styles', 'html', 'img', 'js', 'fonts']);
