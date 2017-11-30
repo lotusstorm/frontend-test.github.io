e9361a4482b8bb9ac9e57276ed5bd5b7c59628a5
lotusstorm <iutizev@gmail.com>
2017-12-01 02:49:38 +0300

    adaptiv-3

---
+++ src/js/menu-button.js
@@ -0,0 +1,27 @@
+const menuButton = doc.getElementById('menu-button'); // eslint-disable-line
+
+menuButton.addEventListener('click', (event) => {
+    const menu = doc.getElementById('menu'); // eslint-disable-line
+    const menuWrapper = doc.getElementById('menu__wrapper'); // eslint-disable-line
+    const menuStyle = menu.style;
+    const ev = event.target;
+
+    if(ev.className == 'menu-button_invisible') {
+        menuWrapper.style.left = '0';
+        menuStyle.width = '100%';
+        menuStyle.zIndex = '4';
+        menuStyle.backgroundColor = 'rgba(31, 34, 41, .5)';
+
+        ev.classList.remove('menu-button_invisible');
+        ev.classList.add('menu-button_visible');
+    }else if(ev.className == 'menu-button_visible') {
+
+        menuWrapper.style.left = '-50%';
+        menuStyle.width = '0';
+        menuStyle.zIndex = '0';
+        menuStyle.backgroundColor = 'rgba(31, 34, 41, .0)';
+
+        ev.classList.remove('menu-button_visible');
+        ev.classList.add('menu-button_invisible');
+    }
+});
--- src/js/menu.js
+++ src/js/menu.js
@@ -2,9 +2,11 @@ const menu = doc.getElementById('menu'); // eslint-disable-line
 
 menu.addEventListener('click', (event) => {
     const menuLink = doc.getElementsByTagName('A'); // eslint-disable-line
-
-    for(let i = 0; i < menuLink.length; i++){
-        menuLink[i].classList.remove(`${event.target.className}_active`);
+    const ev = event.target;
+    if (ev.className != 'menu__link menu__link_active' && ev.className != 'nav__link nav__link_active') {
+        for(let i = 0; i < menuLink.length; i++){
+            menuLink[i].classList.remove(`${ev.className}_active`);
+        }
+        ev.classList.add(`${ev.className}_active`);
     }
-    event.target.classList.add(`${event.target.className}_active`);
 });
