93703ba097466129dd429a82d87a5887ffd39e3a
lotusstorm <iutizev@gmail.com>
2017-12-01 07:45:19 +0300

    fixed commit

--- src/js/menu-button.js
+++ src/js/menu-button.js
@@ -1,8 +1,8 @@
 const menuButton = doc.getElementById('menu-button'); // eslint-disable-line
 
 menuButton.addEventListener('click', (event) => {
-    const menu = doc.getElementById('menu'); // eslint-disable-line
-    const menuWrapper = doc.getElementById('menu__wrapper'); // eslint-disable-line
+    const menu = doc.querySelector('#menu'); // eslint-disable-line
+    const menuWrapper = doc.querySelector('#menu__wrapper'); // eslint-disable-line
     const menuStyle = menu.style;
     const ev = event.target;
 
@@ -10,7 +10,7 @@ menuButton.addEventListener('click', (event) => {
         menuWrapper.style.left = '0';
         menuStyle.width = '100%';
     }else {
-        menuWrapper.style.left = '-50%';
-        menuStyle.width = '0';
+        menuWrapper.style.left = '';
+        menuStyle.width = '';
     }
 });
