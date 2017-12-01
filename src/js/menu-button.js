f711bc8fde636b3fe527a510dfec4c2c2f78848d
lotusstorm <iutizev@gmail.com>
2017-12-01 05:19:09 +0300

    adaptiv-7

--- src/js/menu-button.js
+++ src/js/menu-button.js
@@ -8,19 +8,9 @@ menuButton.addEventListener('click', (event) => {
 
     if(!ev.checked) {
         menuWrapper.style.left = '0';
-        menuStyle.zIndex = '4';
         menuStyle.width = '100%';
-        menuStyle.backgroundColor = 'rgba(31, 34, 41, .5)';
-
-        ev.classList.remove('menu-button_invisible');
-        ev.classList.add('menu-button_visible');
     }else {
         menuWrapper.style.left = '-50%';
-        menuStyle.zIndex = '0';
         menuStyle.width = '0';
-        menuStyle.backgroundColor = 'rgba(31, 34, 41, .0)';
-
-        ev.classList.remove('menu-button_visible');
-        ev.classList.add('menu-button_invisible');
     }
 });
