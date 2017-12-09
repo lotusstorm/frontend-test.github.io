0f9e1258381dfb481e321936fd169165793d3d9f
lotusstorm <iutizev@gmail.com>
2017-12-09 17:19:55 +0300

    fixe-5

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,7 +3,7 @@ const menu = doc.getElementById('menu'); // eslint-disable-line
 menu.addEventListener('click', (event) => {
     const menuLink = doc.getElementsByTagName('A'); // eslint-disable-line
     const ev = event.target;
-    if (ev.tagName == 'menu__link' || ev.className == 'nav__link') { 
+    if (ev.className == 'menu__link' || ev.className == 'nav__link') { 
         for(let i = 0; i < menuLink.length; i++){
             menuLink[i].classList.remove(`${ev.className}_active`);
         }
